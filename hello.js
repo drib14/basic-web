29c9c27530fb9caf3e1abebe300fc2656d884e2a
drib14 <jhondribramirez7@gmail.com>
2024-10-23 17:48:11 +0800

    finishing complete js course

--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
-console.log('hello')
+// Arrays
+
+// Creating arrays
+let hobbies = ["singing", "coding", "sleeping"]
+
+// Array length functionality
+console.log(hobbies.length)
+
+// Reading Array
+// Syntax
+// identifier[index]
+// index starts in 0 && length starts in 1
+console.log(hobbies[0])
+
+try {
+    let hobbies = ["singing", "coding", "sleeping"]
+    console.log(hobbies.length)
+    console.log(hobbies[10].length); 
+} catch(err) {
+    console.error('Error: ',err)
+}
